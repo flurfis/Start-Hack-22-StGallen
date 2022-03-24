949b5b6a31434f852809b459923ab3dc01b80c65
heitzli <luc.heitz@unibas.ch>
2022-03-24 03:12:11 +0100

    test-query and plot

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,3 +1,23 @@
-import psycopg2
+import matplotlib.pyplot as plt
+from connection import Connection
 
-conn = psycopg2.connect(dbname="starthack22", user="starthack", password="starthack22", host="")
+# if running local postgres disable service first
+
+conn = Connection("starthack", "starthack22", "starthack22")
+con = conn.getSQLConnection()
+
+with con.cursor() as cur:
+    cur.execute("select v.start_date, count(v.id) from verlaufe v where v.produkt like 'Sozialhilfe%' "
+                "group by v.start_date order by v.start_date;")
+
+    rows = cur.fetchall()
+    x = [i[0] for i in rows]
+    y = [j[1] for j in rows]
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    fig.show()
+
+    print("test")
+
+conn.stopConnection()
